d5b183c724545b66e0e738d14805a9a44b2543b6
niniedo <edoardo.nini@gmail.com>
2025-10-22 18:27:41 +0200

    first commit

---
+++ src/electron/main.ts
@@ -0,0 +1,28 @@
+import { app, BrowserWindow } from "electron";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  if (!app.isPackaged) {
+    win.loadURL("http://localhost:5173");
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+};
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
